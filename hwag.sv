4016b67c5679f7d316b36399f06393bb4a946681
Ruslan <ep8kha@gmail.com>
2021-01-05 21:06:49 +0500

    метка во время нормального зуба

--- hwag.sv
+++ hwag.sv
@@ -57,12 +57,14 @@ comparator #(24) pcnt3_less_half_pcnt2_comp (.a(pcnt3_data),.b({1'b0,pcnt2_data[
 
 d_flip_flop #(3) comparator_buffer (.clk(clk),.ena(1'b1),   .d({pcnt1_less_half_pcnt,  pcnt1_less_half_pcnt2,  pcnt3_less_half_pcnt2  }),.srst(1'b0),.arst(rstb),
                                                             .q({pcnt1_less_half_pcnt_b,pcnt1_less_half_pcnt2_b,pcnt3_less_half_pcnt2_b}));
-                                                            
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 wire gap_found = pcnt1_less_half_pcnt2_b & pcnt3_less_half_pcnt2_b;
-wire hwag_ena = ~hwag_start &  pcnt_start & gap_found & main_edge;
-wire hwag_srst = hwag_start & ~pcnt_start;
+
+wire gap_drn_normal_tooth = hwag_start & main_edge & ~gap_point & pcnt1_less_half_pcnt_b;
+
+wire hwag_ena = ~hwag_start & pcnt_start & gap_found & main_edge;
+
+wire hwag_srst = hwag_start & (~pcnt_start | gap_drn_normal_tooth); //(!)
 
 d_flip_flop #(1) hwag_enable (.clk(clk),.ena(hwag_ena),.d(1'b1),.srst(hwag_srst),.arst(rstb),.q(hwag_start));
 
@@ -70,7 +72,9 @@ wire [5:0] tcnt_load_data;
 mult2to1 #(6) tcnt_load_sel (.sel(~hwag_start),.a(6'd57),.b(6'd54),.out(tcnt_load_data));
 
 wire tcnt_count = main_edge & hwag_start;
+
 wire tcnt_ena = tcnt_count & ~tcnt_ovf;
+
 wire tcnt_sload = (tcnt_count & tcnt_ovf) | ~hwag_start;
 
 wire [5:0] tcnt_data;
@@ -88,15 +92,19 @@ counter #(18) scnt (.clk(clk),.ena(~tckc_ovf),.sel(1'b1),.sload(hwag_start & (ma
 wire [7:0] tckc_data_load;
 mult2to1 #(8) tckc_data_sel (.sel(pregap_point),.a(8'd64),.b(8'd192),.out(tckc_data_load));
 
+wire tckc_count = ~tckc_ovf & scnt_ovf;
 wire [7:0] tckc_out;
-counter #(8) tckc (.clk(clk),.ena(~tckc_ovf & scnt_ovf),.sel(1'b1),.sload(hwag_start & main_edge),.d_load(tckc_data_load),.srst(1'b0),.arst(rstb),.q(tckc_out),.carry_out(tckc_ovf));
+counter #(8) tckc (.clk(clk),.ena(tckc_count),.sel(1'b1),.sload(hwag_start & main_edge),.d_load(tckc_data_load),.srst(1'b0),.arst(rstb),.q(tckc_out),.carry_out(tckc_ovf));
 
 wire [11:0] acnt_data_load;
 mult2to1 #(12) acnt_data_sel (.sel(acnt_ovf),.a({tcnt_data,6'd0}),.b(12'd3839),.out(acnt_data_load));
 
 wire [11:0] acnt_out;
-wire acnt_ena = hwag_start & ~acnt_ovf & ~tckc_ovf & scnt_ovf;
-wire acnt_sload = (hwag_start & acnt_ovf & ~tckc_ovf & scnt_ovf) | main_edge;
+
+wire acnt_ena = hwag_start & ~acnt_ovf & tckc_count;
+
+wire acnt_sload = (hwag_start & acnt_ovf & tckc_count) | main_edge;
+
 counter #(12) acnt (.clk(clk),.ena(acnt_ena),.sel(1'b1),.sload(acnt_sload),.d_load(acnt_data_load),.srst(1'b0),.arst(rstb),.q(acnt_out),.carry_out(acnt_ovf));
     
 endmodule
