0275e685a82be6eff8ef2af16367f81bb4f23cbe
Ruslan <ep8kha@gmail.com>
2021-01-16 23:40:21 +0500

    gap verification

--- hwag.sv
+++ hwag.sv
@@ -22,7 +22,7 @@ wire rstb;
 wire capb;
 wire camb;
 
-//assign ngap_point = ~gap_point;
+assign ngap_point = ~gap_point;
 
 d_flip_flop #(3) input_buffer (.clk(clk),.ena(1'b1),.d({rst,cap,cam}),.arst(1'b0),.q({rstb,capb,camb}));
     
@@ -42,13 +42,13 @@ wire [23:0] pcnt_data;
 counter #(24) pcnt (.clk(clk),.ena(pcnt_start),.sel(1'b0),.srst(cap_edge),.arst(rstb),.q(pcnt_data),.carry_out(pcnt_ovf));
 
 wire [23:0] pcnt1_data;
-d_flip_flop #(24) pcnt1 (.clk(clk),.ena(cap_edge),.d(pcnt_data),.srst(~pcnt_start),.arst(rstb),.q(pcnt1_data));
+d_flip_flop #(24) pcnt1 (.clk(clk),.ena(cap_edge & ~gap_point),.d(pcnt_data),.srst(~pcnt_start),.arst(rstb),.q(pcnt1_data));
     
 wire [23:0] pcnt2_data;
-d_flip_flop #(24) pcnt2 (.clk(clk),.ena(cap_edge),.d(pcnt1_data),.srst(~pcnt_start),.arst(rstb),.q(pcnt2_data));
+d_flip_flop #(24) pcnt2 (.clk(clk),.ena(cap_edge & ~gap_point),.d(pcnt1_data),.srst(~pcnt_start),.arst(rstb),.q(pcnt2_data));
     
 wire [23:0] pcnt3_data;
-d_flip_flop #(24) pcnt3 (.clk(clk),.ena(cap_edge),.d(pcnt2_data),.srst(~pcnt_start),.arst(rstb),.q(pcnt3_data));
+d_flip_flop #(24) pcnt3 (.clk(clk),.ena(cap_edge & ~gap_point),.d(pcnt2_data),.srst(~pcnt_start),.arst(rstb),.q(pcnt3_data));
 
 wire [23:0] half_pcnt_data = {1'b0,pcnt_data[23:1]};
 wire [23:0] half_pcnt2_data = {1'b0,pcnt2_data[23:1]};
@@ -75,18 +75,35 @@ wire pcnt_less_min =   ~pcnt_min_less_pcnt1 |  ~pcnt_min_less_pcnt2 | ~pcnt_min_
 wire pcnt_nom = pcnt_greater_min & pcnt3_less_pcnt_max;
 wire pcnt_not_nom = pcnt_less_min | (pcnt_max_less_pcnt1 & pcnt_max_less_pcnt2);
 
+wire gap_run = pcnt1_less_half_pcnt;
 wire gap_found = pcnt1_less_half_pcnt2 & pcnt3_less_half_pcnt2;
 
 //========================================================================
 
 wire hwag_start_ena_d = (pcnt_nom & gap_found & pcnt_start) & ~hwag_start;
-wire hwag_start_srst_d = (pcnt_not_nom | ~pcnt_start) & hwag_start;
+wire hwag_start_srst_d = (gap_drn_normal_tooth | gap_lost | pcnt_not_nom | ~pcnt_start) & hwag_start;
 
 //========================================================================
 
 d_flip_flop #(2) hwag_start_ena_srst (.clk(clk),.ena(1'b1),.d({hwag_start_ena_d,hwag_start_srst_d}),.arst(rstb),.q({hwag_start_ena,hwag_start_srst}));
 
 d_flip_flop #(1) hwag_start_trigger (.clk(clk),.ena(hwag_start_ena),.d(1'b1),.srst(hwag_start_srst),.arst(rstb),.q(hwag_start));
+
+
+wire tooth_counter_ena = cap_edge & ~tooth_counter_ovf;
+wire tooth_counter_sload = (cap_edge & tooth_counter_ovf) | ~hwag_start;
+wire [7:0] tooth_counter_d_load;
+
+mult2to1 #(8) tooth_counter_d_load_sel (.sel(~hwag_start),.a(8'd57),.b(8'd55),.out(tooth_counter_d_load));
+wire [7:0] tooth_counter_data;
+
+counter #(8) tooth_counter (.clk(clk),.ena(tooth_counter_ena),.sel(1'b1),.sload(tooth_counter_sload),.d_load(tooth_counter_d_load),.srst(1'b0),.arst(rstb),.q(tooth_counter_data),.carry_out(tooth_counter_ovf));
+
+d_flip_flop #(1) gap_point_trigger (.clk(clk),.ena(cap_edge),.d(tooth_counter_ovf),.srst(~hwag_start),.arst(rstb),.q(gap_point));
+
+d_flip_flop #(1) gap_lost_trigger (.clk(clk),.ena(cap_edge),.d(gap_point & ~gap_run),.srst(~hwag_start),.arst(rstb),.q(gap_lost));
+
+d_flip_flop #(1) gap_drn_normal_tooth_trigger (.clk(clk),.ena(cap_edge),.d(~gap_point & gap_run),.srst(~hwag_start),.arst(rstb),.q(gap_drn_normal_tooth));
  
 endmodule
 
